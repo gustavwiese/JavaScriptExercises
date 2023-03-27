e4e6cf6760564e5b5a810c7696bd39e921d46c62
gustavwiese <gusse12345@gmail.com>
2023-03-27 09:39:08 +0200

    asd

---
+++ array.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const navne = ["Harry", "Ron", "Hermione"];
+
+console.log(navne);
+console.log(navne.length);
+console.log(navne.at(0));
+console.log(navne[1]);
+
+const drengene = navne.slice(0,2)
+console.log(drengene);
